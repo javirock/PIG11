f73a6fef236e2c1136cd661d6a02c0aaa06deb97
Javirock <javirokr@gmail.com>
2024-07-08 02:07:01 -0300

    BASEURL para PEW

--- static/js/crud-products.js
+++ static/js/crud-products.js
@@ -1,4 +1,6 @@
-const BASEURL = 'http://127.0.0.1:5000';
+/*const BASEURL = 'http://127.0.0.1:5000';*/
+
+const BASEURL = 'https://javirokr.pythonanywhere.com/';
 
 /**
  * Función para realizar una petición fetch con JSON.
